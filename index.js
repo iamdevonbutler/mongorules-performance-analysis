d74c97ed9280abf2e46d73ece3c6e02139463763
iamdevonbutler <jpescione@gmail.com>
2017-01-09 18:55:50 -0500

    added tests for mongoose for comparison

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const co = require('co');
 const db = require('mongorules');
 const mongodb = require('mongodb');
 const microtime = require('microtime');
+const mongoose = require('mongoose');
 
 require('./model.users.js');
 
@@ -12,16 +13,26 @@ function removeUsers(db) {
 }
 
 function addUser(db, name, email) {
+  if (name && Object.prototype.toString.call(name) !== '[object String]') return;
+  name = name.toLowerCase().trim();
+
+  if (!email || email === null) return;
+  if (Object.prototype.toString.call(email) !== '[object String]' || email.length < 3) return;
+  email = email.toLowerCase().trim();
+
   var users = db.collection('users');
   return new Promise((resolve, reject) => {
-    users.insert({name, email}, (err, result) => {
+    users.insert({name, email, created: new Date()}, (err, result) => {
       if (err) reject(err);
       resolve(result);
     });
   });
 }
 
-function addUserMongorules(db, name, email) {
+function addUserMongorules(db, name, email, novalidate = false) {
+  if (novalidate) {
+    return db.users.novalidate.insert({account: {name, email}});
+  }
   return db.users.insert({account: {name, email}});
 }
 
@@ -67,20 +78,66 @@ co(function* () {
   var itterations = process.argv[2] ? parseInt(process.argv[2], 10) : 10000;
 
   var mongoTestCallback = (i) => addUser(dbInstance, 'jay'+i, `jay@a${i}.com`);
+  var novalidateTestCallback = (i) => addUserMongorules(db, 'jay'+i, `jay@a${i}.com`, true);
   var mongorulesTestCallback = (i) => addUserMongorules(db, 'jay'+i, `jay@a${i}.com`);
 
   // Regular.
   var mongoTime = yield runTestsInSequence(itterations, mongoTestCallback, 'mongo inserts');
   // yield runTestsInParallel(itterations, mongoTestCallback, 'mongo inserts');
+  yield removeUsers(db);
+
+  // Novalidate.
+  var novalidateTime = yield runTestsInSequence(itterations, novalidateTestCallback, 'novalidate inserts');
+  // yield runTestsInParallel(itterations, mongorulesTestCallback, 'mongrules inserts');
+  yield removeUsers(db);
 
   // Mongorules.
   var mongorulesTime = yield runTestsInSequence(itterations, mongorulesTestCallback, 'mongrules inserts');
   // yield runTestsInParallel(itterations, mongorulesTestCallback, 'mongrules inserts');
+  yield removeUsers(db);
 
-  console.log(`>>> Time inc ${((mongorulesTime - mongoTime) / mongoTime)*100}%`);
+  // Mongoose.
+  mongoose.connect('mongodb://localhost/mongorules-perf-test');
+  mongoose.Promise = global.Promise; // to supress error.
+  var Schema = mongoose.Schema;
+
+  var Users = new Schema({
+    email: {
+      type: String,
+      min: 3,
+    },
+    name: {
+      type: String,
+      min: 3,
+    },
+    created: {
+      type: Date,
+      default: new Date,
+    }
+  });
+  Users.path('email').set(function (v) {
+    return v.toLowerCase().trim();
+  });
+  Users.path('name').set(function (v) {
+    return v.toLowerCase().trim();
+  });
+  var UsersModel = mongoose.model('users', Users);
+  var mongooseTime = yield runTestsInSequence(itterations, () => {
+    var user = new UsersModel();
+    user.name = name;
+    user.email = email;
+    return user.save();
+  }, 'mongoose inserts');
+  yield removeUsers(db);
+
+
+  // Results.
+  console.log(`>>> Time change w/ mongorules ${((mongorulesTime - mongoTime) / mongoTime)*100}%`);
+  console.log(`>>> Time change w/ novalidate ${((novalidateTime - mongoTime) / mongoTime)*100}%`);
+  console.log(`>>> Time change w/ mongoose ${((mongooseTime - mongoTime) / mongoTime)*100}%`);
 
   // Clean up.
-  yield removeUsers(db);
   db.close();
+  mongoose.connection.close()
 
 }).catch(e => console.log(e));
